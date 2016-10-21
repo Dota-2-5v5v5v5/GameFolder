0f2d3d8c186ccec2e52c58d5a67563566321887f
Unknown <superdotaplaya@gmail.com>
2016-10-21 00:10:33 -0400

    increased gold per second

--- scripts/vscripts/settings.lua
+++ scripts/vscripts/settings.lua
@@ -10,7 +10,7 @@ PRE_GAME_TIME = 30.0                    -- How long after people select their he
 POST_GAME_TIME = 30.0                  -- How long should we let people look at the scoreboard before closing the server automatically?
 TREE_REGROW_TIME = 60.0                 -- How long should it take individual trees to respawn after being cut down/destroyed?
 
-GOLD_PER_TICK = 1                    -- How much gold should players get per tick?
+GOLD_PER_TICK = 1.5                    -- How much gold should players get per tick?
 GOLD_TICK_TIME = 1                      -- How long should we wait in seconds between gold ticks?
 
 RECOMMENDED_BUILDS_DISABLED = false     -- Should we disable the recommened builds for heroes
